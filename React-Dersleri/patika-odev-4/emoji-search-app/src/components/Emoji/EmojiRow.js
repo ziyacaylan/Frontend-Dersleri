bf3463c59343e8495a976b2db0a78635e4ef61f1
ziyacaylan <ziyacaylan@gmail.com>
2022-10-24 18:00:01 +0300

    patika-odev-4 testler tamamlandı

--- React-Dersleri/patika-odev-4/emoji-search-app/src/components/Emoji/EmojiRow.js
+++ React-Dersleri/patika-odev-4/emoji-search-app/src/components/Emoji/EmojiRow.js
@@ -11,16 +11,16 @@ const EmojiRow = ({ emoji, idx }) => {
       <div
         className="text"
         key={idx}
-        data-clipboard-text={emoji?.symbol}
-        data-testid={"row"}
+        data-clipboard-text={emoji.symbol}
+        data-testid="row"
       >
         <span className="symbol">{emoji?.symbol}</span>
         <span className="title">{emoji?.title}</span>
-      </div>
 
-      <span className="copy" onClick={() => handleClick(emoji.symbol)}>
-        Copy to clipboard
-      </span>
+        <span className="copy" onClick={() => handleClick(emoji.symbol)}>
+          Copy to clipboard
+        </span>
+      </div>
     </li>
   );
 };
--- React-Dersleri/patika-odev-4/emoji-search-app/src/components/Emoji/EmojiRow.test.js
+++ React-Dersleri/patika-odev-4/emoji-search-app/src/components/Emoji/EmojiRow.test.js
@@ -10,28 +10,10 @@ describe("Emojies Results testing", () => {
     render(<App />);
   });
 
-  test("copy", () => {
-    const copytext = screen.getAllByText("Copy to clipboard");
-    userEvent.click(copytext);
-    const input = screen.getByRole("textbox");
-    userEvent.paste(input, copytext);
-    expect(input.length === 1);
+  it("Copying emoji should work successfully", () => {
+    const click = screen.getAllByText("copy");
+    userEvent.click(click);
+    expect(screen.getAllByTestId("row")).toBeInTheDocument();
+    //expect(Click[0]).toHaveAttribute("data-clipboard-text");
   });
-
-  // test("Copying emoji should work successfully", () => {
-  // const click = screen.getAllByTestId("row");
-  // expect(click[0]).toHaveAttribute("data-clipboard-text");
-  // const copytext = screen.getAllByText("Copy to clipboard");
-  // fireEvent.change(copytext, { target: { value: "smile" } });
-  //const input = screen.getByRole("textbox");
-  //userEvent.paste(input, copytext);
-  //expect(input.length === 1);
-  //   jest.spyOn(navigator.clipboard, "writeText");
-  //   beforeAll(() => {
-  //     // yourImplementationThatWouldInvokeClipboardWriteText();
-  //   });
-  //   it("should call clipboard.writeText", () => {
-  //     expect(navigator.clipboard.writeText).toHaveBeenCalledWith("smile");
-  //   });
-  // });
 });
