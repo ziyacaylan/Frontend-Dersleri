0e009dfbbca35a9cc800653b57293e359831b048
ziyacaylan <ziyacaylan@gmail.com>
2022-11-15 15:56:23 +0300

    ecommerce-app | home page harita güncellendi

--- React-Dersleri/ecommerce-app/client/src/Pages/Home/index.js
+++ React-Dersleri/ecommerce-app/client/src/Pages/Home/index.js
@@ -224,10 +224,9 @@ function Home() {
             resmin, heykel gibi belli oranda birebir kopyası elde edilir.
           </Text>
         </Box>
-
-        <Box className={styles.map}>
+        <Box>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96320.74916655212!2d28.649985440984736!3d41.02474374700395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caa727fbb7a5af%3A0x36e1fd9eda6fa4da!2zQXZjxLFsYXIvxLBzdGFuYnVs!5e0!3m2!1str!2str!4v1660121056193!5m2!1str!2str"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4257.446478387751!2d29.178395397106858!3d41.014988106369394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cacee228283919%3A0xe975904936db8bd9!2sSur%20Yap%C4%B1%20Metrogarden%20AVM!5e0!3m2!1str!2str!4v1668516768594!5m2!1str!2str"
             width="100%"
             height="400"
             title="İstanbul"
