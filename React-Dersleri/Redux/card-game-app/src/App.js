39657bfb770e62d96dd1618aa9ee9a791fc8d123
ziyacaylan <ziyacaylan@gmail.com>
2023-02-04 00:17:10 +0300

    work on footer

--- React-Dersleri/Redux/card-game-app/src/App.js
+++ React-Dersleri/Redux/card-game-app/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <h1 className="text-4xl text-violet-500 text-center mt-8">Selam Naber</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ React-Dersleri/Redux/card-game-app/src/redux/cardSlice/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cardSlice = createSlice({
+  name: "card",
+  initialState: {},
+  reducers: {},
+});
+export default cardSlice.reducer;
